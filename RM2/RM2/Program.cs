d76b0b6103c0215aa8492a9c3fd60e043f3b84f7
mayn <mayn@DESKTOP-BR09NBL>
2019-03-29 22:16:39 +0800

    hello I‘m 奔跑的鱼

--- RM2/RM2/Program.cs
+++ RM2/RM2/Program.cs
@@ -10,6 +10,12 @@ namespace RM2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
